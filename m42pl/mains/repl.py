6806b49fbec1b3120517cef76b3b57233bdef1dc
jpclipffel <jpclipffel@users.noreply.github.com>
2021-05-03 20:48:57 +0200

    Rewrite built-ins matching and execution

--- m42pl/mains/repl.py
+++ m42pl/mains/repl.py
@@ -1,5 +1,6 @@
 import readline
 import regex
+import sys
 import os
 from pathlib import Path
 
@@ -25,7 +26,10 @@ class REPL(RunAction):
 
     dispatcher_alias = 'local_repl'
     prompt = 'm42pl | '
-    history_file = Path(os.environ.get('HOME'), '.m42pl_history')
+    history_file = Path(os.environ.get('HOME'), '.m42pl_history') # type: ignore
+
+    # Builtins command regex
+    regex_builtins = regex.compile(r'^(?P<name>exit|modules)(\s.*)?$', flags=regex.IGNORECASE)
 
     def __init__(self, *args, **kwargs):
         super().__init__('repl', *args, **kwargs)
@@ -50,6 +54,20 @@ class REPL(RunAction):
         ] + [None,]
         return matched[state]
 
+    def builtin_exit(self):
+        sys.exit(0)
+
+    def builtin_modules(self):
+        print(f'{os.linesep}Imported modules')
+        for name, module in m42pl.modules.items():
+            print(f'* {name}: {module}')
+        print(f'{os.linesep}Modules imported by name')
+        for name in m42pl.IMPORTED_MODULES_NAMES:
+            print(f'* {name}')
+        print(f'{os.linesep}Modules imported by path')
+        for name in m42pl.IMPORTED_MODULES_PATHS:
+            print(f'* {name}')
+
     def __call__(self, args):
         super().__call__(args)
         # Build aliases list for completer
@@ -57,10 +75,10 @@ class REPL(RunAction):
             alias for alias, _
             in m42pl.commands.ALIASES.items()
         ]
-        # Select and connect KVStore
-        kvstore = m42pl.kvstore(args.kvstore)()
         # Select and instanciate dispatcher
-        dispatcher = m42pl.dispatcher(args.dispatcher)()
+        dispatcher = m42pl.dispatcher(args.dispatcher)(**args.dispatcher_kwargs)
+        # Select and connect KVStore
+        kvstore = m42pl.kvstore(args.kvstore)(**args.kvstore_kwargs)
         # Read history file
         if args.history:
             self.history_file = Path(args.history)
@@ -74,19 +92,25 @@ class REPL(RunAction):
                 # Read and cleanup script
                 source = input(self.prompt).lstrip(' ').rstrip(' ')
                 if len(source):
-                    # Built-in command: exit
-                    if regex.match(r'^exit(\s.*)?$', source, flags=regex.IGNORECASE):
-                        break
-                    # Build-int command: reload
-                    if regex.match(r'^reload(\s.*)?$', source, flags=regex.IGNORECASE):
-                        print(f'reloaded: {", ".join(m42pl.reload_modules())}')
-                    # Built-in command: help
-                    elif regex.match(r'^help(\s.*)?$', source, flags=regex.IGNORECASE):
-                        print('No help yet')
-                    # M42PL command
+                    # Try to interpret source as builtin
+                    rx = self.regex_builtins.match(source)
+                    if rx:
+                        getattr(self, f'builtin_{rx.groupdict()["name"]}')()
+
+                    # # Built-in command: exit
+                    # if regex.match(r'^exit(\s.*)?$', source, flags=regex.IGNORECASE):
+                    #     break
+                    # # Build-int command: reload
+                    # if regex.match(r'^reload(\s.*)?$', source, flags=regex.IGNORECASE):
+                    #     print(f'reloaded: {", ".join(m42pl.reload_modules())}')
+                    # # Built-in command: help
+                    # elif regex.match(r'^help(\s.*)?$', source, flags=regex.IGNORECASE):
+                    #     print('No help yet')
+
+                    # Otherwise, interpret source as a M42PL pipeline
                     else:
                         readline.write_history_file(self.history_file)
-                        dispatcher(source, kvstore, Event())
+                        dispatcher(source, kvstore, Event(args.event))
             except Exception as error:
                 print(CLIErrorRender(error, source).render())
                 if args.raise_errors:
