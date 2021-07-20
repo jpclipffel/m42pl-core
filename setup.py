239f110fb11e44efc5b4608ad08e5a48116db98a
jpclipffel <jpclipffel@users.noreply.github.com>
2021-07-20 19:47:07 +0200

    Added packages files and setuptools types

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ setup(
   url='https://github.com/jpclipffel/m42pl-core',
   version='0.0.1',
   packages=['m42pl',],
+  package_data={
+      '': ['m42pl/files/*',]
+  },
   entry_points={
     'console_scripts': [
       'm42pl=m42pl.__main__:main'
@@ -15,6 +18,7 @@ setup(
   install_requires=[
     'regex',
     'jsonpath-ng',
-    'lark-parser[regex]'
-  ]
+    'lark-parser[regex]',
+    'types-setuptools',
+  ],
 )
