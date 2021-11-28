cb2ddb3ee484a99db355b842b1b9223751780e4e
jpclipffel <jpclipffel@users.noreply.github.com>
2021-11-28 19:31:22 +0100

    [Add] Added `serve` main and docstrings

--- m42pl/mains/__init__.py
+++ m42pl/mains/__init__.py
@@ -3,12 +3,14 @@ from .run import Run
 from .grammar import Grammar
 from .parse import Parse
 from .status import Status
+from .serve import Serve
 
 
 commands = [
-    REPL,
-    Run,
-    Grammar,
-    Parse,
-    Status
+    REPL,       # Shell-like CLI
+    Run,        # Run script from file
+    Grammar,    # Dump M42PL grammar
+    Parse,      # Parse a M42PL script
+    Status,     # Debug - Dump pipelines status from KVStore
+    Serve       # Test - Run pipelines from REST API
 ]
